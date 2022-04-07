64da689537dcdb6b53a812b2d02a56494f40e82b
ORead6 <92927225+ORead6@users.noreply.github.com>
2022-04-07 23:15:32 +0100

    Created XMl for Account Creation - Need to add transitions between screens

--- MyMood/app/src/main/java/com/example/mymood/loginActivity.java
+++ MyMood/app/src/main/java/com/example/mymood/loginActivity.java
@@ -2,14 +2,18 @@ package com.example.mymood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class loginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-        System.out.println("Gay");
+
     }
+
 }
