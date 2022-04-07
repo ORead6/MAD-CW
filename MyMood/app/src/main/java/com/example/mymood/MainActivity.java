81f01c2ab8a7e5e578e4e2de9b98ad11cb28261e
ORead6 <92927225+ORead6@users.noreply.github.com>
2022-04-07 21:39:07 +0100

    Created Login Screen

---
+++ MyMood/app/src/main/java/com/example/mymood/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mymood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+        System.out.println("Gay");
+    }
+}
